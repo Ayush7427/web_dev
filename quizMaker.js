1d2f655e042e4bba01f9951deab6583ac9c02e1a
Ayush7427 <ayushrana7427@gmail.com>
2023-05-12 09:13:03 +0530

    added new

--- quizMaker.js
+++ quizMaker.js
@@ -1,18 +1,18 @@
 const quizDB = [{
-    question : "Q1) what is the full form of HTML",
-    a : "qwerty hi", 
-    b : "Hyper txt mar" ,
-    c : "none of these" , 
-    d : "Hyper Text Markup Language" ,
-    ans : "ans4"
+    question : "Q1) What do you call a computer on a network that requests files from another computer?",
+    a : "A client", 
+    b : "A host" ,
+    c : " A router" , 
+    d : "A web server" ,
+    ans : "ans1"
 } ,
 {
-    question : "Q2) FULL Form of CSS",
-    a : "qwerty hii", 
-    b : " Casading Style Sheet" ,
-    c : "txt mar",
-    d : "none of these" , 
-    ans : "ans2"
+    question : "Q2) Hardware devices that are not part of the main computer system and are often added later to the system.",
+    a : "Peripheral", 
+    b : " Clip art" ,
+    c : " Highlight",
+    d : "Execute" , 
+    ans : "ans1"
 },
 {
     question : "Q3) Which of the following is not JavaScript Data Types?",
@@ -21,6 +21,22 @@ const quizDB = [{
     c : "Boolean" ,
     d : "Float" ,
     ans : "ans4"
+},
+{
+    question : "Q4) The main computer that stores the files that can be sent to computers that are networked together is",
+    a : "Clip art",
+    b : " Mother board" ,
+    c : " Peripheral" ,
+    d : "File server" ,
+    ans : "ans4"
+},
+{
+    question : "Q5) Google (www.google.com) is a:",
+    a : "Clip art",
+    b : " Search Engine" ,
+    c : " Directory of images" ,
+    d : "File server" ,
+    ans : "ans2"
 }]
 
 const question = document.getElementById("question")
