056bc756ef1e961e67c3473f59d6fe4e34fa2ddd
AYUSH RANA <124423779+Ayush7427@users.noreply.github.com>
2023-07-14 08:41:11 +0530

    Delete email_Password.js

--- email_Password.js
+++
@@ -1,9 +0,0 @@
-function  otp() {
-    let OTP =''
-    let x ='/*7891#'
-    for(let i = 0; i <= 6; i++){
-        OTP += x[Math.floor(Math.random() * 6)]
-    }
-    return OTP
-}
-console.log(otp());
